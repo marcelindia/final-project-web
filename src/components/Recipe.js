1775107dc3715bfbd10c50ecf540c038df78f31f
Diana Marcelin <marcelindiana@yahoo.com>
2022-03-09 14:10:34 -0500

    ingredient list showing in li

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -17,7 +17,7 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: "background.paper",
+  // bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
@@ -26,9 +26,7 @@ const style = {
 function Recipe() {
   const [recipe, setRecipe] = useState([]);
   const [open, setOpen] = React.useState(false);
-  const [ingredient, setIngredient] = useState([]);
-  const [id, setId] = useState("");
-  console.log(id);
+  const [ingredientList, setIngredientList] = useState([]);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -38,34 +36,43 @@ function Recipe() {
       .then((data) => setRecipe(data))
       .catch(alert);
   }, []);
-  const handleOnClick = () => {
-    console.log(id);
-    if (id) {
-      fetch(`https://lets-eat-71558.uk.r.appspot.com/recipes/${id}`)
-        .then((response) => response.json())
-        .then((data) => setIngredient(data))
-        .then(() => handleOpen())
-        .catch(alert);
-    }
+
+  const handleOnClick = (recipeId) => {
+    fetch(`https://lets-eat-71558.uk.r.appspot.com/recipes/${recipeId}`)
+      .then((response) => response.json())
+      .then((data) => setIngredientList(data))
+      .then(() => handleOpen())
+      .catch(alert);
   };
-  console.log(ingredient);
+
   return (
     <div>
       <Modal
         open={open}
-        onClose={handleClose}
+        // onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            <h2 style={{ backgroundColor: "lavender" }}>{ingredient.title}</h2>
+            <header style={{ backgroundColor: "lavender" }}>
+              {ingredientList.title}
+            </header>
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {ingredient.ingredients}
-          </Typography>
+
+          {ingredientList?.ingredients?.map((ig) => {
+            console.log(ig);
+            return (
+              <ul className="list-group">
+                <li>{ig}</li>
+              </ul>
+            );
+          })}
+
+          <Button onClick={handleClose}>Close</Button>
         </Box>
       </Modal>
+
       {!recipe ? (
         <h2>Loading...</h2>
       ) : (
@@ -73,51 +80,24 @@ function Recipe() {
           {recipe.map((recipe) => {
             return (
               <Card>
-                <CardContent
-                  style={{
-                    marginTop: 50,
-                    marginBottom: 50,
-                    marginLeft: 50,
-                    marginRight: 50,
-                    backgroundColor: "paleturquoise",
-                  }}
-                >
+                <CardContent>
                   <h2 style={{ backgroundColor: "lavender" }}>
                     {recipe.title}
                   </h2>
-                  <img src={recipe.img} width="300" height="400" />
+                  <h3 style={{ backgroundColor: "#51d1e1" }}>
+                    <img src={recipe.img} width="300" height="400" />
+                  </h3>
                   <CardActionArea>
                     <Button
                       onClick={() => {
-                        setId(recipe.id);
                         if (recipe.id) {
-                          handleOnClick();
+                          handleOnClick(recipe.id);
+                          console.log(recipe);
                         }
                       }}
                     >
                       Ingredients
                     </Button>
-                    {/* <Modal
-                      open={open}
-                      onClose={handleClose}
-                      aria-labelledby="modal-modal-title"
-                      aria-describedby="modal-modal-description"
-                    >
-                      <Box sx={style}>
-                        <Typography
-                          id="modal-modal-title"
-                          variant="h6"
-                          component="h2"
-                        >
-                          <h2 style={{ backgroundColor: "lavender" }}>
-                            {recipe.title}
-                          </h2>
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                          {recipe.ingredients}
-                        </Typography>
-                      </Box>
-                    </Modal> */}
                   </CardActionArea>
                 </CardContent>
               </Card>
